7450f36e0fe33d00f948c3049219fe542cd6c1ad
Jared R. Kannianen <98085138+JarKanni@users.noreply.github.com>
2023-03-11 01:54:14 -0600

    formatted StartTime and reordered columns
    - StartTime now reads as 'Month 01 @ 09 AM'
    - columns now ordered: Round, StartTime, Race, Location

--- schedule.py
+++ schedule.py
@@ -1,23 +1,28 @@
 import fastf1
 import pandas as pd
+from pandas.tseries.offsets import DateOffset
+from datetime import datetime as dt
 
+##Load Data##
 # enable cache for faster loading
 fastf1.Cache.enable_cache('C:/Users/Admin/Coding/sports')
 # load full season schedule from fastf1
-schedule_full = fastf1.get_event_schedule(2023)
+sch = fastf1.get_event_schedule(2023, include_testing = False)
 
 ##Cleaning##
 # add Country into Location
-schedule_full['Location'] = schedule_full['Location'] + ', ' + schedule_full['Country']
-
+sch['Location'] = sch['Location'] + ', ' + sch['Country']
 # convert EventDate to central time and change name to 'StartTime'
-schedule_full['EventDate'] = schedule_full.EventDate + pd.DateOffset(hours = -11)
+sch['EventDate'] = sch.EventDate + pd.DateOffset(hours = -11)
+# format StartTime to 'Month 01 @ 9AM'
+sch['EventDate'] = sch['EventDate'].apply(lambda x: x.strftime('%B %d @ %I %p'))
+# rename columns
+sch.rename(columns = {'RoundNumber':'Round' , 'EventName':'Race', 'EventDate':'StartTime'}, inplace = True)
 
 
 ## Main schedule ##
 # build schedule dataframe
-schedule_full.rename(columns = {'RoundNumber':'Round' , 'EventDate':'StartTime' , 'EventName':'Race'}, inplace = True)
-schedule = schedule_full[['Round','Race','Location','StartTime']]
+schedule = sch[['Round', 'StartTime', 'Race','Location']]
 schedule = schedule.set_index('Round')
 
 
